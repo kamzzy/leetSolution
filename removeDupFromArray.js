0b5719f7ce520781e078b43f4c1334aac6f9cdea
Precious Akams <preciousakams@yahoo.com>
2023-02-20 21:46:55 +0100

    remove duplicate

--- removeDupFromArray.js
+++ removeDupFromArray.js
@@ -4,3 +4,15 @@
 // Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
 
 // Return k after placing the final result in the first k slots of nums.
+
+var removeDuplicates = function(nums) {
+  if(nums.length < 2) return nums.length
+  let count = 0
+  for( let i = 0; i < nums.length; i++){
+    if(nums[count] !== nums[i]) {
+      count++
+      nums[count] = nums[i]
+    }
+  }
+  return count+1
+}
